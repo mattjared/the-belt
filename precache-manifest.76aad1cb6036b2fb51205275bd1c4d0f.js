b140dedc51cbc16d8ab270404613ddff23449137
mattjared <mattjared9@gmail.com>
2019-01-12 11:10:21 -0600

    Updates

---
+++ precache-manifest.76aad1cb6036b2fb51205275bd1c4d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b936e287937da49fca4",
+    "url": "/static/js/main.7b936e28.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "7b936e287937da49fca4",
+    "url": "/static/css/main.f274a403.chunk.css"
+  },
+  {
+    "revision": "fc5dd784917104adb6eb33392daf2064",
+    "url": "/index.html"
+  }
+];
